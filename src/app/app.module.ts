973f5ce2df2d4a8c7950b300218a5f12500742d1
jacopo-marcolini-bid <jacopo.marcolini@bidcompany.it>
2022-04-01 18:04:44 +0200

    [add] blockchain & settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
+import { BlockViewComponent } from './components/block-view/block-view.component';
+import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockchainViewerComponent,
+    BlockViewComponent,
+    TransactionsTableComponent,
+    SettingsComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
